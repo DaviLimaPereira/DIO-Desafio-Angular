a920eff5cc56adb31be5d7eacbd4ac9339e7dd6e
DaviLimaPereira <davilimapereira@hotmail.com>
2022-07-23 14:07:48 -0300

    Produtos

--- Desafio-Angular_ECommerce/src/app/app.module.ts
+++ Desafio-Angular_ECommerce/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NavComponent } from './components/shared/nav/nav.component';
 import { BookstoreAppComponent } from './components/bookstore-app/bookstore-app.component';
 import { FiltersComponent } from './components/bookstore-app/filters/filters.component';
 import { ProductListComponent } from './components/bookstore-app/product-list/product-list.component';
+import { ProductItemComponent } from './components/bookstore-app/product-list/product-item/product-item.component';
+import { BooksService } from './components/bookstore-app/product-list/product-list.component.service';
 
 @NgModule({
   declarations: [
@@ -18,13 +20,14 @@ import { ProductListComponent } from './components/bookstore-app/product-list/pr
     NavComponent,
     BookstoreAppComponent,
     FiltersComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [BooksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Desafio-Angular_ECommerce/src/app/components/bookstore-app/product-list/product-list.component.ts
+++ Desafio-Angular_ECommerce/src/app/components/bookstore-app/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Book } from './model/Book';
+import { BooksService } from './product-list.component.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  livros: any;
+  booksService: BooksService;
+
+  constructor(  booksService: BooksService) {
+
+    this.booksService = booksService;
+
+    // this.booksService = new BooksService();
+   }
 
   ngOnInit(): void {
+
+    this.livros = this.booksService.getBooks().subscribe((data => {
+      this.livros = data;
+      console.log(this.livros);
+    }))
+
   }
 
+
 }
+
+
